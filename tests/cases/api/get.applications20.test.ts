37cb22714602ed2cb374e8bbfe6acca5297aa932
FilipMucka <f.mucka@gmail.com>
2019-10-02 15:19:17 +0200

    Update tests

--- tests/cases/api/get.applications20.test.ts
+++ tests/cases/api/get.applications20.test.ts
@@ -1,48 +1,34 @@
 import supertest from 'supertest'
 import app from '../../../src/app'
+const request = supertest(app)
 
-const endpoint = '/api/v1/applications'
-const { env } = <any>process
-const today = new Date()
-
-describe(`[GET] ${endpoint})`, () => {
-
+describe(`[GET] /`, () => {
 	it('Bad endpoint, response should return code 404', async () => {
-		const request = supertest(app)
-		const response = await request.get(endpoint)
-			.set('Content-Type', 'application/json')
+		const response = await request.get('').set('Content-Type', 'application/json')
 
 		expect(response.status).toBe(404)
 	})
 
 	it('Bad endpoint, response should return code 404', async () => {
-		const request = supertest(app)
-		const response = await request.get(endpoint)
-			.set('Content-Type', 'application/json')
+		const response = await request.get('').set('Content-Type', 'application/json')
 
 		expect(response.status).toBe(404)
 	})
 
 	it('Bad endpoint, response should return code 404', async () => {
-		const request = supertest(app)
-		const response = await request.get(endpoint)
-			.set('Content-Type', 'application/json')
+		const response = await request.get('').set('Content-Type', 'application/json')
 
 		expect(response.status).toBe(404)
 	})
 
 	it('Bad endpoint, response should return code 404', async () => {
-		const request = supertest(app)
-		const response = await request.get(endpoint)
-			.set('Content-Type', 'application/json')
+		const response = await request.get('').set('Content-Type', 'application/json')
 
 		expect(response.status).toBe(404)
 	})
 
 	it('Bad endpoint, response should return code 404', async () => {
-		const request = supertest(app)
-		const response = await request.get(endpoint)
-			.set('Content-Type', 'application/json')
+		const response = await request.get('').set('Content-Type', 'application/json')
 
 		expect(response.status).toBe(404)
 	})
